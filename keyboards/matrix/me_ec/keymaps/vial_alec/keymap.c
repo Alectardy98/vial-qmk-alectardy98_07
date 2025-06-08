465b629d04ee37d27c0169f18927ecea25e9a8c0
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2025-06-08 19:26:28 -0400

    finish matrix me_ec

--- keyboards/matrix/me_ec/keymaps/vial_alec/keymap.c
+++ keyboards/matrix/me_ec/keymaps/vial_alec/keymap.c
@@ -1,13 +1,19 @@
 #include QMK_KEYBOARD_H
+#include "ch.h"
+#include "hal.h"
+#include "rgblight.h"
 
-// Defines names for use in layer keycodes and the keymap
+
+
+
 enum layer_names {
     _BASE,
     _FN
 };
 
-enum custom_keycodes {      //In Mac OS Swap Command and Control for proper macro use
-    TEST = SAFE_RANGE,
+// Defines the keycodes used by our macros in process_record_user
+enum blender_keycode {
+    TEST = QK_KB_0,
     VDRT,                   //Desktop Right "set to move right a space on mac"
     VDLT,                   //Desktop Left "set to move left a space on mac"
     VDUP,                   //V-Desktop Up "set to mission controll on mac"
@@ -26,218 +32,243 @@ enum custom_keycodes {      //In Mac OS Swap Command and Control for proper macr
     LPRC,                   //The "(" Key
     RPRC,                   //The ")" Key
     NUM,                    //Num Lock for both mac and windows
-    //Control Section
-    CTLA,                   //CTRL + A
-    CTLS,                   //CTRL + S
-    CTLD,                   //CTRL + D
-    CTLF,                   //CTRL + F
-    CTLZ,                   //CTRL + Z
-    CTLX,                   //CTRL + X
-    CTLC,                   //CTRL + C
-    CTLV,                   //CTRL + V
     //Discord Section
     THIS,                   //Macro for ":this:" emoji on discord
     OOF,                    //Macro for ":oof:" emoji on discord
     PIKA,                   //Macro for ":surprised:" emoji on discord
-    SCAT,                   //Macro for ":smiley_cat_spanish:" emoji on discord
-    FCAT,                   //Macro for ":smiley_cat_fancy:" emoji on discord
+    SCAT,                   //Macro for ":smiley_cat2:" emoji on discord
+    FCAT,                   //Macro for ":smiley_cat1:" emoji on discord
     HART,                   //Macro for ":heart:" emoji on discord
     DROL,                   //Macro for ":drooling_face:" emoji on discord
-    MONY,                   //Macro for ":smiley_takemoney:" emoji on discord
-    FHAT,                   //Macro for "::smiley_tip_fedora:" emoji on discord
-    SPIT,                   //Macro for ":smiley_spit_surprised:" emoji on discord
-    KING,                   //Macro for ":pepe_king_royal:" emoji on discord
-
+    MONY,                   //Macro for ":smiley_take:" emoji on discord
+    FHAT,                   //Macro for ":smiley_fedora:" emoji on discord
+    SPIT,                   //Macro for ":smiley_spit:" emoji on discord
+    KING,                   //Macro for ":pepe_king:" emoji on discord
+    FLEX,                   //Macro for ":muscle:" emoji on discord
+    HAHA,                   //Macro for ":smiley_kekw:" emoji on discord
+    LCRY,                   //Macro for ":joy:" emoji on discord
+    NICE,                   //Macro for ":nice~2:" emoji on discord
+    PPOG,                   //Macro for ":pepe_pog:" emoji on discord
+    PRAY,                   //Macro for ":pray_tone2:" emoji on discord
+    SWET,                   //Macro for ":sweat_smile:" emoji on discord
+    COLD,                   //Macro for ":cold_face:" emoji on discord
+    THUM,                   //Macro for ":thumbsup:" emoji on discord
+    //RGB Heart
+    HRT1,                   //Set badge Heart back to default
+    HRT2,                   //Set badge Heart Rainbow
+    HRT3,                   //Set badge Snake
 };
     
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (record->event.pressed) {             //Code for macros
-        switch(keycode) {
+    if (record->event.pressed) {             // Code for macros
+        switch (keycode) {
             case TEST:
-            if (record->event.pressed) {
                 SEND_STRING("TEST");
-                }break;
+                break;
             case VDRT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI( SS_DOWN(X_LCTRL) SS_TAP(X_RIGHT) SS_UP(X_LCTRL) ));
-                }break;
+                SEND_STRING(SS_LGUI(SS_DOWN(X_LCTL) SS_TAP(X_RIGHT) SS_UP(X_LCTL)));
+                break;
             case VDLT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI( SS_DOWN(X_LCTRL) SS_TAP(X_LEFT) SS_UP(X_LCTRL) ));
-                }break;
+                SEND_STRING(SS_LGUI(SS_DOWN(X_LCTL) SS_TAP(X_LEFT) SS_UP(X_LCTL)));
+                break;
             case VDUP:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LGUI(SS_TAP(X_TAB)"t"));
-                }break;
+                SEND_STRING(SS_LGUI(SS_TAP(X_TAB) SS_TAP(X_T)));
+                break;
             case VDDN:
-            if (record->event.pressed) {
                 SEND_STRING(SS_LGUI("d"));
-                }break;
+                break;
             case EXPL:
-            if (record->event.pressed) {
                 SEND_STRING(SS_LGUI("e"));
-                }break;
+                break;
             case SNIP:
-            if (record->event.pressed) {
                 SEND_STRING(SS_LGUI(SS_LSFT("s")));
-                }break;
+                break;
             case COPY:
-            if (record->event.pressed) {
-                SEND_STRING(SS_TAP(X_F2) SS_LCTRL("ac"));
-                }break;
+                SEND_STRING(SS_TAP(X_F2) SS_LCTL(SS_TAP(X_A) SS_TAP(X_C)));
+                break;
             case CUTT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_TAP(X_F2) SS_LCTRL("ax"));
-                }break;
+                SEND_STRING(SS_TAP(X_F2) SS_LCTL(SS_TAP(X_A) SS_TAP(X_X)));
+                break;
             case PAST:
-            if (record->event.pressed) {
-                SEND_STRING(SS_TAP(X_F2) SS_LCTRL("v"));
-                }break;
+                SEND_STRING(SS_TAP(X_F2) SS_LCTL(SS_TAP(X_V)));
+                break;
             case TASK:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL(SS_DOWN(X_RSHIFT)SS_TAP(X_ESCAPE)SS_UP(X_RSHIFT)));       // Windows
-                SEND_STRING(SS_LCTRL(SS_DOWN(X_LALT)SS_TAP(X_ESCAPE)SS_UP(X_LALT)));           //Mac
-                }break;
+                SEND_STRING(SS_LCTL(SS_DOWN(X_RSFT) SS_TAP(X_ESCAPE) SS_UP(X_RSFT)));  // Windows
+                SEND_STRING(SS_LCTL(SS_DOWN(X_LALT) SS_TAP(X_ESCAPE) SS_UP(X_LALT)));    // Mac
+                break;
             case GOGL:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL(SS_LALT("g")));
-                }break;
+                SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_G))));
+                break;
             case CMD:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL(SS_LALT("t")));
-                }break;
+                SEND_STRING(SS_LCTL(SS_LALT(SS_TAP(X_T))));
+                break;
             case QUIT:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LALT(SS_TAP(X_F4)));                                          //Windows
-                SEND_STRING(SS_LCTRL("w"));                                                   //Mac
-                }break;
+                SEND_STRING(SS_LALT(SS_TAP(X_F4)));     // Windows
+                SEND_STRING(SS_LCTL(SS_TAP(X_W)));      // Mac
+                break;
             case DZRO:
-            if (record->event.pressed) {
                 SEND_STRING("00");
-                }break;
+                break;
             case ATAB:
-            if (record->event.pressed) {
                 SEND_STRING(SS_LALT(SS_TAP(X_TAB)));
-                }break;
+                break;
             case LPRC:
-            if (record->event.pressed) {
                 SEND_STRING("(");
-                }break;
+                break;
             case RPRC:
-            if (record->event.pressed) {
                 SEND_STRING(")");
-                }break;
-//            case NUM:
-//            if (record->event.pressed) {
-//                SEND_STRING( SS_TAP(X_NLCK) );                  //Toggles layer change and presses the num lock button, to allow for Num lock toggle to occure for both Mac OS and Windows
-//                layer_invert(_NUM);
-//            return false;
-//            }break;
-                //Control Section
-            case CTLA:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("a"));
-                }break;
-            case CTLS:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("s"));
-                }break;
-            case CTLD:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("d"));
-                }break;
-            case CTLF:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("f"));
-                }break;
-            case CTLZ:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("z"));
-                }break;
-            case CTLX:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("x"));
-                }break;
-            case CTLC:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("c"));
-                }break;
-            case CTLV:
-            if (record->event.pressed) {
-                SEND_STRING(SS_LCTRL("v"));
-                }break;
-                //Discord Section
+                break;
+            case NUM:
+                SEND_STRING(SS_TAP(X_NUM));         // Toggles layer change and presses the num lock button, to allow for Num lock toggle to occur for both Mac OS and Windows
+                return true;
+                break;
+            // Discord Section
             case THIS:
-            if (record->event.pressed) {
-                    SEND_STRING(":this:" SS_TAP(X_ENTER) );
-                    }break;
+                SEND_STRING(":this:" SS_TAP(X_ENTER));
+                break;
             case OOF:
-            if (record->event.pressed) {
-                    SEND_STRING(":oof:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":oof:" SS_TAP(X_ENTER));
+                break;
             case PIKA:
-            if (record->event.pressed) {
-                    SEND_STRING(":surprised:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":surprised:" SS_TAP(X_ENTER));
+                break;
             case SCAT:
-            if (record->event.pressed) {
-                    SEND_STRING(":smiley_cat_spanish:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":smiley_cat2:" SS_TAP(X_ENTER));
+                break;
             case FCAT:
-            if (record->event.pressed) {
-                    SEND_STRING(":smiley_cat_fancy:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":smiley_cat1:" SS_TAP(X_ENTER));
+                break;
             case HART:
-            if (record->event.pressed) {
-                    SEND_STRING(":heart:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":heart:" SS_TAP(X_ENTER));
+                break;
             case DROL:
-            if (record->event.pressed) {
-                    SEND_STRING(":drooling_face:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":drooling_face:" SS_TAP(X_ENTER));
+                break;
             case MONY:
-            if (record->event.pressed) {
-                    SEND_STRING(":smiley_takemoney:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":smiley_take:" SS_TAP(X_ENTER));
+                break;
             case FHAT:
-            if (record->event.pressed) {
-                    SEND_STRING(":smiley_tip_fedora:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":smiley_fedora:" SS_TAP(X_ENTER));
+                break;
             case SPIT:
-            if (record->event.pressed) {
-                    SEND_STRING(":smiley_spit_surprised:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":smiley_spit:" SS_TAP(X_ENTER));
+                break;
             case KING:
-            if (record->event.pressed) {
-                    SEND_STRING(":pepe_king_royal:" SS_TAP(X_ENTER));
-                    }break;
+                SEND_STRING(":pepe_king:" SS_TAP(X_ENTER));
+                break;
+            case FLEX:
+                SEND_STRING(":muscle:" SS_TAP(X_ENTER));
+                break;
+            case HAHA:
+                SEND_STRING(":smiley_kekw:" SS_TAP(X_ENTER));
+                break;
+            case LCRY:
+                SEND_STRING(":joy:" SS_TAP(X_ENTER));
+                break;
+            case NICE:
+                SEND_STRING(":nice~2:" SS_TAP(X_ENTER));
+                break;
+            case PPOG:
+                SEND_STRING(":pepe_pog:" SS_TAP(X_ENTER));
+                break;
+            case PRAY:
+                SEND_STRING(":pray_tone2:" SS_TAP(X_ENTER));
+                break;
+            case SWET:
+                SEND_STRING(":sweat_smile:" SS_TAP(X_ENTER));
+                break;
+            case COLD:
+                SEND_STRING(":cold_face:" SS_TAP(X_ENTER));
+                break;
+            case THUM:
+                SEND_STRING(":thumbsup:" SS_TAP(X_ENTER));
+                break;
+            case HRT1:
+                rgblight_mode(RGBLIGHT_MODE_BREATHING);
+                rgblight_sethsv(200, 255, 255);
+                rgblight_set_speed(2);
+                return false;
+            case HRT2:
+                rgblight_mode(RGBLIGHT_MODE_RAINBOW_MOOD);
+                rgblight_set_speed(2);
+                return false;
+            case HRT3:
+                rgblight_mode(RGBLIGHT_MODE_SNAKE);
+                rgblight_set_speed(0);
+                return false;
         }
     }
-#ifdef CONSOLE_ENABLE //Console Debug
-    uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %1u, time: %u, interrupt: %1u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+
+#ifdef CONSOLE_ENABLE // Console Debug
+    uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %1u, time: %u, interrupt: %1u, count: %u\n",
+            keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time,
+            record->tap.interrupted, record->tap.count);
 #endif
+
     return true;
-    
-};
+}
+
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_default(
-                             
-		 KC_ESC,            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,          KC_PSCR, KC_SLCK, KC_PAUS,
+    [_BASE] = LAYOUT(
 
-		 KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,           KC_INS, KC_HOME, KC_PGUP,
-		 KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,           KC_DEL,  KC_END, KC_PGDN,
-		KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,           KC_ENT,
-		KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,          KC_RSFT,                     KC_UP,
-		KC_LCTL, MO(_FN), KC_LALT,                             KC_SPC,                                     KC_RALT, KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
+         KC_ESC,            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,          KC_PSCR, KC_SCRL, KC_PAUS,
 
-    [_FN] = LAYOUT_default(
-                           
-        QK_BOOT,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______,          _______, _______, _______,
+         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,           KC_INS, KC_HOME, KC_PGUP,
+         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSLS,           KC_DEL,  KC_END, KC_PGDN,
+        KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,           KC_ENT,
+        KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,          KC_RSFT,                     KC_UP,
+        KC_LCTL, MO(_FN), KC_LALT,                             KC_SPC,                                     KC_RALT, KC_RGUI, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT),
 
-		_______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,          _______, _______, _______,
-        _______,    THIS,     OOF,    PIKA,    SCAT,    FCAT,    HART,    DROL,    MONY,    FHAT,    SPIT, _______, _______, _______,          _______, _______, _______,
-		KC_LCTL,    CTLA,    CTLS,    CTLD,    CTLF, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU,          _______,
-		_______,             CTLZ,    CTLX,    CTLC,    CTLV, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,          _______,                      VDUP,
-		KC_LGUI, _______, _______,                            _______,                                     _______, _______, _______,             VDLT,    VDDN,    VDRT),
+    [_FN] = LAYOUT(
+
+        QK_BOOT,          UG_PREV, UG_NEXT, UG_HUEU, UG_HUED, UG_SATU, UG_SATD, UG_VALU, UG_VALD, UG_SPDU, UG_SPDD, _______, _______,             HRT1,    HRT2,    HRT3,
+
+        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_DEL,          _______, _______, _______,
+           SNIP,    THIS,     OOF,    PIKA,    SCAT,    FCAT,    HART,    DROL,    MONY,    FHAT,    SPIT, _______, _______, _______,          _______, _______, _______,
+        KC_LCTL, C(KC_A), C(KC_S), C(KC_D), C(KC_F), _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU,          _______,
+        _______,          C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,          _______,                      VDUP,
+        KC_LGUI, _______, _______,                            _______,                                     _______, _______, _______,             VDLT,    VDDN,    VDRT),
+
 };
+
+//——— RGBLIGHT Lighting Layers ——–
+#define RGBLAYER_CAPS 0
+#define RGBLAYER_FN   1
+
+// Caps Lock layer: white on all LEDs
+const rgblight_segment_t PROGMEM capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    { 0, RGBLIGHT_LED_COUNT, HSV_WHITE }
+);
+
+// _FN layer: cyan on all LEDs
+const rgblight_segment_t PROGMEM fn_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    { 0, RGBLIGHT_LED_COUNT, HSV_CYAN }
+);
+
+// Lighting layers list (later overrides earlier)
+const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    capslock_layer,
+    fn_layer
+);
+
+// Initialize RGBLIGHT and attach layers
+void keyboard_post_init_user(void) {
+    rgblight_enable();             // turn on lights
+    rgblight_layers = rgb_layers;  // enable our layers
+}
+
+// Sync Caps Lock key state to lighting
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(RGBLAYER_CAPS, led_state.caps_lock);
+    return true;
+}
+
+// Sync _FN layer state to lighting
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(RGBLAYER_FN, layer_state_cmp(state, _FN));
+    return state;
+}
+
+
--- keyboards/matrix/me_ec/keymaps/vial_alec_old/rules.mk
+++ keyboards/matrix/me_ec/keymaps/vial_alec_old/rules.mk
@@ -1,7 +1,12 @@
 VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIAL_INSECURE = yes
+LTO_ENABLE = yes
+
+RGBLIGHT_ENABLE = yes
 VIALRGB_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = IS31FL3731
 
 
 DYNAMIC_MACRO_ENABLE = yes
@@ -10,8 +15,8 @@ TAP_DANCE_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 QMK_SETTINGS = yes
 UNICODE_ENABLE = yes
-BOOTMAGIC_ENABLE = no # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
