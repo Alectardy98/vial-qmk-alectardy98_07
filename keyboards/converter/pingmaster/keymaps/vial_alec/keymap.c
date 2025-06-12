ce1f23fcbb92f5f3abb67855c08a5028e2a89cc5
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2025-06-12 16:04:38 -0400

    Pingmaster done

--- keyboards/converter/pingmaster/keymaps/vial_alec/keymap.c
+++ keyboards/converter/pingmaster/keymaps/vial_alec/keymap.c
@@ -222,11 +222,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
 	[_BASE] = LAYOUT(
-     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,  KC_DEL,    SNIP,      KC_INS, KC_HOME, KC_PGUP,      KC_NUM, KC_PSLS, KC_PAST, KC_PEQL,  \
+     KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,  KC_DEL,    SNIP,      KC_INS, KC_HOME, KC_PGUP,     KC_PCMM, KC_PSLS, KC_PAST, KC_PEQL,  \
      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,             EXPL,      KC_DEL,  KC_END, KC_PGDN,        KC_7,    KC_8,    KC_9, KC_PPLS,  \
-    KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_GRV,  KC_ENT,             PAST,     KC_MPRV, KC_MPLY, KC_MNXT,        KC_1,    KC_5,    KC_6, KC_PMNS,  \
+    KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_GRV,  KC_ENT,             PAST,     KC_MPRV, KC_MPLY, KC_MNXT,        KC_4,    KC_5,    KC_6, KC_PMNS,  \
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_RSFT,                      COPY,     KC_VOLD,   KC_UP, KC_VOLU,        KC_1,    KC_2,    KC_3, KC_BSPC,  \
-    KC_LCTL, MO(_FN), KC_LALT,                                      KC_SPC,                                     KC_RALT, KC_LGUI, KC_RCTL,              KC_LEFT, KC_DOWN, KC_RGHT,       KC_P0,    DZRO, KC_PDOT, KC_PENT   \
+    KC_LCTL, MO(_FN), KC_LALT,                                      KC_SPC,                                     KC_RALT, KC_LGUI, KC_RCTL,              KC_LEFT, KC_DOWN, KC_RGHT,        KC_0,    DZRO, KC_PDOT, KC_PENT   \
         ),
     
     [_FN] = LAYOUT(
