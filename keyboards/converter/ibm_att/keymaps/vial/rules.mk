20eb9a96f129d89158128d1cb3629b26ff238304
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2025-06-13 18:52:39 -0400

    att update

--- keyboards/converter/ibm_att/keymaps/vial/rules.mk
+++ keyboards/converter/ibm_att/keymaps/vial/rules.mk
@@ -4,16 +4,16 @@ VIAL_INSECURE = yes
 LTO_ENABLE = yes
 
 
-DYNAMIC_MACRO_ENABLE = no
+DYNAMIC_MACRO_ENABLE = yes
 COMBO_ENABLE = yes
-TAP_DANCE_ENABLE = no
+TAP_DANCE_ENABLE = yes
 KEY_OVERRIDE_ENABLE = no
 QMK_SETTINGS = no
 UNICODE_ENABLE = yes
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = no      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes         # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 AUDIO_ENABLE = no
