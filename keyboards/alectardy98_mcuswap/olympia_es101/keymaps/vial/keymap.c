9cd450e8272366fccdbbc645810d6329d416b3b1
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2026-02-14 16:52:46 -0500

    olympia firmware finished

--- keyboards/alectardy98_mcuswap/olympia_es101/keymaps/vial/keymap.c
+++ keyboards/alectardy98_mcuswap/olympia_es101/keymaps/vial/keymap.c
@@ -35,18 +35,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  KC_LEFT, KC_RGHT,
-        KC_LGUI, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   _______,    _______,
-        _______,  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,           KC_PGUP, _______,
-        KC_LCTL, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                              KC_PGDN,
-                          KC_LALT,                   KC_SPC,                   KC_RALT,                              KC_DOWN, KC_UP
+     KC_ESC,      KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, _______, KC_BSPC,    KC_LEFT, KC_RGHT,
+    KC_LGUI,      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,                      _______, _______,
+    _______,     KC_LSFT,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, _______, KC_ENT,              KC_PGUP, _______,
+    KC_LCTL,              KC_BSLS,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,                               KC_PGDN, _______, _______, _______,
+                                            KC_LALT,                    KC_SPC,                   KC_RALT,                                                 KC_DOWN, KC_UP
     ),
 
     [_FN] = LAYOUT(
-        KC_ESC,  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC, KC_DEL,  KC_LEFT, KC_RGHT,
-        KC_LGUI, KC_TAB,  _______, KC_UP,   _______, _______, KC_END,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______,                  XXX,    XXX,
-        _______,  KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, KC_BSLS, KC_ENT,           KC_PGUP, _______,
-        KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                              KC_PGDN,
-                          KC_LALT,                   KC_SPC,                   KC_RALT,                              KC_DOWN, KC_UP
+    _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______,
+    _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                      _______, _______,
+    _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______,
+    _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                               _______, _______, _______, _______,
+                                            _______,                   _______,                   _______,                                                 _______, _______
     ),
 };
