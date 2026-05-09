36b1398e312b0fb93fa69c08437b15bb6f08b44b
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2026-05-08 23:36:24 -0400

    update for zenithal rev3pico

--- keyboards/alectardy98/zenithal/keymaps/vial_rev3pico/keymap.c
+++ keyboards/alectardy98/zenithal/keymaps/vial_rev3pico/keymap.c
@@ -27,15 +27,6 @@ enum layer_names {
     _FN
 };
 
-bool led_update_kb(led_t led_state) {
-    if (!led_update_user(led_state)) {
-        return false;
-    }
-    writePin(GP11, led_state.scroll_lock);
-    writePin(GP13, led_state.num_lock);
-    writePin(GP28, led_state.caps_lock);
-    return true;
-}
 
 
 enum blender_keycode {
@@ -69,7 +60,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
-          KC_F1,    KC_F2,    KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,  KC_GRV,TG(_NUM), _______,    BEEP,
+          KC_F1,    KC_F2,    KC_ESC,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSPC,  KC_GRV,TG(_NUM), _______, CK_TOGG,
           KC_F3,    KC_F4,    KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,  KC_ENT, KC_HOME,   KC_UP, KC_PGUP, KC_PMNS,
           KC_F5,    KC_F6,    KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, _______,          KC_LEFT, _______, KC_RGHT, KC_PPLS,
           KC_F7,    KC_F8,    KC_LSFT, _______,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT, KC_BSLS,  KC_END, KC_DOWN, KC_PGDN, KC_PENT,
